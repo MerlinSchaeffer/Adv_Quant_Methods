f2c1ba16ab6d0defca4b3429bb3424818ead5cd6
Merlin Schaeffer <merlin.schaeffer@posteo.de>
2022-08-12 16:07:05 +0200

    Lecture 3 done.

--- static/Lectures/3-RCT/Prep_APAX.R
+++ static/Lectures/3-RCT/Prep_APAX.R
@@ -35,22 +35,40 @@ APAX <- read_dta("./static/Lectures/3-RCT/APAX.dta") %>% # Read the APAX data,
     dis_public = case_when(
       dis_public == -1  | dis_public == 6 ~ as.numeric(NA),
       TRUE ~ zap_labels(dis_public)),
+    antidiscr_law = case_when(
+      antidiscr_law == -1  | antidiscr_law == 6 ~ as.numeric(NA),
+      TRUE ~ zap_labels(antidiscr_law)),
     imor = as_factor(imor),
+    imor = case_when(
+      imor == "Mainstream" ~ "Child of immigrant",
+      TRUE ~ as.character(imor)) %>% factor(),
     gender = as_factor(gender),
+    gender = case_when(
+      gender == "Divers" ~ "Weiblich",
+      TRUE ~ as.character(gender)) %>% factor(),
     appearance = case_when(
       race == -1 & bpoc == 1 ~ as.character(NA), 
       (race == -1 & bpoc == 2) | race == 5 ~ "White",
       race == 1 ~ "Arabic",
       race == 2 ~ "Asian",
       race == 3 ~ "Black",
-      race == 4 ~ "Southern")) %>%
-  filter(imor != "Mainstream", gender != "Divers") %>%
+      race == 4 ~ "Southern"),
+    article = case_when(
+      article == 2 ~ "Treat_1",
+      article == 3 ~ "Treat_2",
+      article == 4 ~ "Contr"),
+    # Average discrimination across domains.
+    dis_index = rowmeans( #<<
+      dis_trainee, dis_job, dis_school, 
+      dis_house, dis_gov, dis_public, 
+      na.rm = TRUE)) %>%
+  filter(corigin != "Österreich") %>%
   mutate(
     gender = fct_drop(gender),
     imor = fct_drop(imor),
     appearance =  fct_drop(appearance)) %>%
-  select(news_hrs, news_mins, starts_with("dis_"), gewFAKT, gender, age, imor, 
-         german, nbh_exposed, appearance)
+  select(antidiscr_law, dis_index, news_hrs, news_mins, starts_with("dis_"), 
+         gewFAKT, gender, age, imor, german, nbh_exposed, appearance, article)
 
 save(APAX, file = "./static/Lectures/3-RCT/APAX.RData")
 load("./static/Lectures/3-RCT/APAX.RData")
